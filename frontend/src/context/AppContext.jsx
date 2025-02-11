115986fb761767346d0bf2fe391c8980417ce34e
Anusha <anus88152@gmail.com>
2025-02-11 21:41:22 +0530

    updated

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,9 +4,10 @@ export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [showLogin,setShowLogin]=useState(false);
 
   return (
-    <AppContext.Provider value={{ user, setUser }}>
+    <AppContext.Provider value={{ user, setUser,showLogin,setShowLogin }}>
       {children}
     </AppContext.Provider>
   );
